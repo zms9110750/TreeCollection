8f42d9a1a820f66f6078ef890f72b0b3fd5ee84f
1622989459@qq.com <1622989459@qq.com>
2025-03-09 02:38:42 +0800

    完善WM的API

---
+++ Abstract/ITreeList.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+
+namespace zms9110750.TreeCollection.Abstract;
+
+public interface ITreeList<T> : IList<T>
+{
+	/// <summary>
+	/// 添加一堆项目到指定位置
+	/// </summary>
+	/// <param name="index">要放置的位置</param>
+	/// <param name="item">要添加的项目</param>
+	void AddAt(Index index, params IEnumerable<T> item);
+	/// <summary>
+	/// 添加一个项目到指定位置
+	/// </summary>
+	/// <param name="index">要放置位置</param>
+	/// <param name="item">要添加的项目</param>
+	/// <returns>添加进去的值</returns>
+	T AddAt(Index index, T item);
+	/// <summary>
+	/// 查询项目是否存在于该列表中
+	/// </summary>
+	/// <param name="item">查询项目</param>
+	/// <param name="range">查询范围</param>
+	/// <returns>若存在则为true，否则为false。</returns>
+	bool Contains(T item, Range? range = null);
+	/// <summary>
+	/// 查询项目在列表中的位置
+	/// </summary>
+	/// <param name="item">查找项目</param>
+	/// <param name="range">查询范围</param>
+	/// <returns>项目的索引。若不存在，则返回-1。</returns>
+	int IndexOf(T item, Range? range = null);
+	/// <summary>
+	/// 移除第一个匹配的的项目
+	/// </summary>
+	/// <param name="item">查找项目</param>
+	/// <param name="range">查询范围</param>
+	/// <returns>被移除的项目。若没有找到，则返回null。</returns>
+	T? Remove(T item, Range? range = null);
+	/// <summary>
+	/// 移除符合条件的项目
+	/// </summary>
+	/// <param name="match">条件</param>
+	/// <param name="range">仅筛选以下范围</param>
+	/// <returns>被移除的项目列表</returns>
+	List<T> RemoveAll(Predicate<T>? match = null, Range? range = null);
+	/// <summary>
+	/// 移除指定位置的项目
+	/// </summary>
+	/// <param name="index">要移除的位置</param>
+	/// <returns></returns>
+	T RemoveAt(Index index);
+
+	IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+	int IList<T>.IndexOf(T item) => IndexOf(item, default);
+
+	void IList<T>.Insert(int index, T item) => AddAt(index, item);
+
+	void IList<T>.RemoveAt(int index) => RemoveAt(index);
+
+	void ICollection<T>.Add(T item) => AddAt(^0, item);
+
+	void ICollection<T>.Clear() => RemoveAll();
+
+	bool ICollection<T>.Contains(T item) => Contains(item, default);
+
+	bool ICollection<T>.Remove(T item)
+	{
+		var index = IndexOf(item);
+		if (index >= 0)
+		{
+			RemoveAt(index);
+			return true;
+		}
+		return false;
+	}
+}
--- Abstract/TreeList.cs
+++ Abstract/TreeList.cs
@@ -1,19 +1,12 @@
-﻿using System.Collections;
-using zms9110750.TreeCollection.Extension;
+﻿using zms9110750.TreeCollection.Extension;
 
 namespace zms9110750.TreeCollection.Abstract;
-public abstract class TreeList<T> : IList<T>
+public abstract class TreeList<T> : ITreeList<T>
 {
 	/// <summary>
 	/// 内部集合
 	/// </summary>
-	protected List<T> InnerList { get; } = [];
-	/// <summary>
-	/// 添加一个项目到指定位置
-	/// </summary>
-	/// <param name="index">要放置位置</param>
-	/// <param name="item">要添加的项目</param>
-	/// <returns>添加进去的值</returns>
+	protected List<T> InnerList { get; } = []; 
 	public virtual T AddAt(Index index, T item)
 	{
 		ValidateElement(item);
@@ -28,11 +21,7 @@ public abstract class TreeList<T> : IList<T>
 		}
 		return item;
 	}
-	/// <summary>
-	/// 添加一堆项目到指定位置
-	/// </summary>
-	/// <param name="index">要放置的位置</param>
-	/// <param name="item">要添加的项目</param>
+	/// <inheritdoc cref="AddAt(Index, IEnumerable{T})" /> 
 	public virtual void AddAt(Index index, params ReadOnlySpan<T> item)
 	{
 		foreach (var i in item)
@@ -42,18 +31,22 @@ public abstract class TreeList<T> : IList<T>
 		var count = index.GetOffset(InnerList.Count);
 		if (count == Count)
 		{
+#if NET8_0_OR_GREATER
+
 			InnerList.AddRange(item);
+#else
+			InnerList.AddRange(item.ToArray());
+#endif 
 		}
 		else
 		{
+#if NET8_0_OR_GREATER
 			InnerList.InsertRange(count, item);
-		}
+#else
+			InnerList.InsertRange(count, item.ToArray());
+#endif
+		} 
 	}
-	/// <summary>
-	/// 添加一堆项目到指定位置
-	/// </summary>
-	/// <param name="index">要放置的位置</param>
-	/// <param name="item">要添加的项目</param>
 	public virtual void AddAt(Index index, IEnumerable<T> item)
 	{
 		foreach (var i in item)
@@ -70,23 +63,12 @@ public abstract class TreeList<T> : IList<T>
 			InnerList.InsertRange(count, item);
 		}
 	}
-	/// <summary>
-	/// 移除指定位置的项目
-	/// </summary>
-	/// <param name="index">要移除的位置</param>
-	/// <returns></returns>
 	public virtual T RemoveAt(Index index)
 	{
 		var item = this[index];
 		InnerList.RemoveAt(index.GetOffset(InnerList.Count));
 		return item;
 	}
-	/// <summary>
-	/// 移除符合条件的项目
-	/// </summary>
-	/// <param name="match">条件</param>
-	/// <param name="range">仅筛选以下范围</param>
-	/// <returns>被移除的项目列表</returns>
 	public virtual List<T> RemoveAll(Predicate<T>? match = default, Range? range = default)
 	{
 		List<T> list = [];
@@ -98,7 +80,11 @@ public abstract class TreeList<T> : IList<T>
 				break;
 			case (null, { } r):
 				var (off, len) = r.GetOffsetAndLength(InnerList.Count);
+#if NET8_0_OR_GREATER
 				list.AddRange(InnerList.Slice(off, len));
+#else
+				list.AddRange(InnerList.GetRange(off, len));
+#endif
 				InnerList.RemoveRange(off, len);
 				break;
 			case ({ } m, null):
@@ -126,36 +112,18 @@ public abstract class TreeList<T> : IList<T>
 		}
 		return list;
 	}
-	/// <summary>
-	/// 移除第一个匹配的的项目
-	/// </summary>
-	/// <param name="item">查找项目</param>
-	/// <param name="range">查询范围</param>
-	/// <returns>被移除的项目。若没有找到，则返回null。</returns>
 	public virtual T? Remove(T item, Range? range = default)
 	{
 		var index = IndexOf(item, range);
 		return index >= 0 ? RemoveAt(index) : default;
 	}
-	/// <summary>
-	/// 查询项目是否存在于该列表中
-	/// </summary>
-	/// <param name="item">查询项目</param>
-	/// <param name="range">查询范围</param>
-	/// <returns>若存在则为true，否则为false。</returns>
 	public abstract bool Contains(T item, Range? range = default);
-	/// <summary>
-	/// 查询项目在列表中的位置
-	/// </summary>
-	/// <param name="item">查找项目</param>
-	/// <param name="range">查询范围</param>
-	/// <returns>项目的索引。若不存在，则返回-1。</returns>
 	public abstract int IndexOf(T item, Range? range = default);
 	/// <summary>
 	/// 验证元素是否合法。
 	/// </summary>
 	/// <remarks>若不合法则抛出异常。</remarks>
-	/// <param name="item">严重项目</param>
+	/// <param name="item">验证项目</param>
 	protected abstract void ValidateElement(T item);
 	public abstract T this[int index] { get; set; }
 	/// <summary>
@@ -178,49 +146,4 @@ public abstract class TreeList<T> : IList<T>
 		return ((IEnumerable<T>)InnerList).GetEnumerator();
 	}
 
-	IEnumerator IEnumerable.GetEnumerator()
-	{
-		return ((IEnumerable)InnerList).GetEnumerator();
-	}
-
-	int IList<T>.IndexOf(T item)
-	{
-		return IndexOf(item, default);
-	}
-
-	void IList<T>.Insert(int index, T item)
-	{
-		AddAt(index, item);
-	}
-
-	void IList<T>.RemoveAt(int index)
-	{
-		RemoveAt(index);
-	}
-
-	void ICollection<T>.Add(T item)
-	{
-		AddAt(^0, item);
-	}
-
-	void ICollection<T>.Clear()
-	{
-		RemoveAll();
-	}
-
-	bool ICollection<T>.Contains(T item)
-	{
-		return Contains(item, default);
-	}
-
-	bool ICollection<T>.Remove(T item)
-	{
-		var index = IndexOf(item);
-		if (index >= 0)
-		{
-			RemoveAt(index);
-			return true;
-		}
-		return false;
-	}
 }
